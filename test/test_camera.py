9cb80d7d0a3eb4473bec98a0899899e50300802d
klaus-moser <klaus-moser@posteo.de>
2022-11-17 20:40:18 +0100

    Add basis testfile + gitkeep

---
+++ test/test_camera.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestCamera:
+    """
+    Test class for 'camera.py'.
+    """
+
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
